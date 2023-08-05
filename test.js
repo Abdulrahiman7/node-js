697c56b70d7cfe490194fd8bff847246ea4e12ff
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-08-05 21:49:58 +0530

    Server response 3 url

--- test.js
+++ test.js
@@ -1,6 +1,23 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    console.log('Abdul Rahiman');
+    switch(req.url)
+    {
+        case '/home':
+        res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><h1>Welcome to the Home Page</h1></html></body>');
+    res.end();
+    break;
+    case '/about':
+        res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><h1>Welcome to About Page</h1></html></body>');
+    res.end();
+    break;
+    case '/node':
+        res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body><h1>Welcome to Node js project</h1></html></body>');
+    res.end();
+    break;
+    }
 })
 server.listen(4000);
   
