2348d2f61f99b02588ee5a2ebad500db0a06eaaa
Abdulrahiman7 <rahimanabdul981@gmail.com>
2023-08-08 07:20:54 +0530

    Exporting and importing

--- test.js
+++ test.js
@@ -1,44 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const reqHandler=require('./serv')
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    try {
-      const data = fs.readFileSync('./messag.txt', { encoding: 'utf-8' });
-      res.setHeader('Content-Type', 'text/html');
-      res.write('<html>');
-      res.write(`<body>${data}</body>`);
-      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>');
-      res.write('</html>');
-      return res.end();
-    } catch (err) {
-      console.log('Error reading file:', err);
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'text/html');
-      res.write('<html><body><h1>Error: Could not read file</h1></body></html>');
-      return res.end();
-    }
-  }
-
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      console.log(parsedBody);
-      fs.writeFileSync('./messag.txt', message);
-      res.statusCode = '302';
-      res.setHeader('Location', '/');
-      return res.end();
-    });
-  }
-});
+const server = http.createServer(reqHandler);
 
 server.listen(3000);
